c0516fc5f4f208e3c9a538eb8c37c363c184621e
amnasyed1 <amna.syed@stonybrook.edu>
2023-08-18 14:46:31 +0000

    ive added the variables and their meanings

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # health-analytics
 This HHA 506 assignment is a primer for HHA 504/507 classes that will be taken in the fall. 
+
+```
+variable = 98
+```
+
+# the variables I created
+for the number varibale i created the patient's age, which is 22
+for the string variable i created the patient's name, which is Taylor
+for the list varibale i created health conditions of the patient  = [migranes, asthma, allergies] 
+for the dictionary i created the patient's health history
--- health_analysis.py
+++ health_analysis.py
@@ -1,3 +1,19 @@
 import pandas as pd
 import numpy as np 
 
+#variables
+patient_age = 22
+patient_firstName = Taylor 
+patient_healthconditions = [migranes, asthma, allergies]
+patient_information = {
+    "age" : "22"
+    "name' : "Taylor"
+    "patient_pastInformation" : {
+        "current health conditions" : [migranes, asthma, allergies],
+        "current medications" : { 
+            "migranes" : "triptan"
+            "asthma" : "singuliar"
+            "allergies" : "cetirizine"
+    }
+    }
+    
